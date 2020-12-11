3415abca06012fed96e11cf665cbadebe98d67e5
Chelsea Becker <cschweig2@gmail.com>
2020-12-11 11:23:34 -0800

    add welcome message to UI

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -1,11 +1,19 @@
 using System;
-using Bread.Models;
-using Pastry.Models;
+using Bakery.Models;
 
 namespace Bakery
 {
     public class Program
     {
         public double price = 0;
+
+        public static void Main()
+        {
+            Console.WriteLine("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
+            Console.WriteLine("Hello! Welcome to Pierre's Bakery!");
+            Console.WriteLine("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
+
+
+        }
     }
 }
