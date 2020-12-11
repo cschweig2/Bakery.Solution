5ac2b24fc7105e746499418037424832f917e885
Chelsea Becker <cschweig2@gmail.com>
2020-12-11 13:52:37 -0800

    add TotalPriceBread() method

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -7,15 +7,21 @@ namespace Bakery
     {
         public static double orderTotal = 0;
 
+        public static void TotalPriceBread()
+        {
+            
+        }
+
         public static void OrderBread()
         {
             Bread bread = new Bread();
             Console.WriteLine("How many loaves of bread would you like to order? They are $5 each, or 2 for $6.");
             int orderNum = int.Parse(Console.ReadLine());
-            Console.WriteLine("You have ordered " + orderNum + " loaves of bread. Would you like to complete your bread order? ('Yes' or 'No')");
+            Console.WriteLine("You have ordered " + orderNum + " loaves of bread. Would you like to complete your order of bread? ('Yes' or 'No')");
             string answer = Console.ReadLine();
-            if (answer == "Yes")
+            if (answer == "Yes" || answer == "No")
             {
+                // get total to complete order
                 if (orderNum % 2 == 0)
                 {
                     bread.SetPrice(3);
@@ -29,29 +35,70 @@ namespace Bakery
                 {
                     int breadPrice = bread.GetPrice();
                     int totalBread = breadPrice * orderNum;
-                    Console.WriteLine("Your subtotal is $" + totalBread + ".");
+                    double convertTotal = System.Convert.ToDouble(totalBread);
+                    orderTotal += convertTotal;
+                    Console.WriteLine("Your subtotal is $" + orderTotal + ".");
                 }
-                
+                // ask if would like pastries
                 Console.WriteLine("Would you like to add pastries to your order? They are $2 each, or 2 for $3.('Yes' or 'No')");
                 string answer2 = Console.ReadLine();
                 if (answer2 == "Yes")
                 {
-                    // pastries function
+                    OrderPastries();
                 }
-                
-                End();
-            }
-            else if (answer == "No")
-            {
-                Console.WriteLine("How many more loaves would you like to add to your order?");
-                orderNum = int.Parse(Console.ReadLine());
+                else if (answer2 == "No")
+                {
+                    End();
+                }
             }
         }
+
         public static void OrderPastries()
         {
             Pastry pastry = new Pastry();
             Console.WriteLine("How many pastries would you like to order? They are $2 each, or 2 for $3.");
+            int orderNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("You have ordered " + orderNum + " pastries. Would you like to complete your order of pastries? ('Yes' or 'No')");
+            string answer = Console.ReadLine();
+            if (answer == "Yes" || answer == "No")
+            {
+                if (orderNum % 2 == 0)
+                {
+                    pastry.SetPrice(1.50);
+                    double pastryPrice = pastry.GetPrice();
+                    double totalPastry = pastryPrice * System.Convert.ToDouble(orderNum);
+                    orderTotal += totalPastry;
+                    Console.WriteLine("Your subtotal is $" + orderTotal + ".");
+                }
+                else
+                {
+                    double pastryPrice = pastry.GetPrice();
+                    double totalPastry = pastryPrice * System.Convert.ToDouble(orderNum);
+                    orderTotal += totalPastry;
+                    Console.WriteLine("Your subtotal is $" + orderTotal + ".");
+                }
+            }
+            Console.WriteLine("Will that complete your order? ('Yes' or 'No')");
+            string answer2 = Console.ReadLine();
+            if (answer2 == "Yes")
+            {
+                End();
+            }
+            else if (answer2 == "No")
+            {
+                Console.WriteLine("What else can I get for you today? ('Bread' or 'Pastries')");
+                string answer3 = Console.ReadLine();
+                if (answer == "Bread")
+                {
+                    OrderBread();
+                }
+                else if (answer == "Pastries")
+                {
+                    OrderPastries();
+                }
+            }
         }
+
         public static void End()
         {
             Console.WriteLine("That will be $" + orderTotal + ", please!");
@@ -63,17 +110,17 @@ namespace Bakery
             Console.WriteLine("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
             Console.WriteLine("Hello! Welcome to Pierre's Bakery!");
             Console.WriteLine("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
-            Console.WriteLine("Would you like to place an order for bread or pastries? ('bread' or 'pastries')");
+            Console.WriteLine("Would you like to place an order for bread or pastries? ('Bread' or 'Pastries')");
             string orderChoice = Console.ReadLine();
 
-            if (orderChoice == "bread")
+            if (orderChoice == "Bread")
             {
                 OrderBread();
             }
-            // else if (orderChoice == "pastries")
-            // {
-            //     orderPastries();
-            // }
+            else if (orderChoice == "Pastries")
+            {
+                OrderPastries();
+            }
         }
     }
 }
