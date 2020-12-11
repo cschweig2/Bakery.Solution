712dbf102cc5c7c8635ddc1505a0a776799a8424
Chelsea Becker <cschweig2@gmail.com>
2020-12-11 09:10:42 -0800

    test 1 PASS

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -1,1 +1,18 @@
+using Bakery.Models;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Bakery.Tests
+{
+    [TestClass]
+    public class BakeryTests
+    {
+        [TestMethod]
+        public void BreadConstructor_CreatesInstanceOfBread_Bread()
+        {
+            Bread bread = new Bread();
+            Assert.AreEqual(typeof(Bread), bread.GetType());
+        }
+    }
+}
