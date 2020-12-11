2f5080410b7673ed179cebf0b28502359b888619
Chelsea Becker <cschweig2@gmail.com>
2020-12-11 11:45:59 -0800

    add user choice for bread or pastry

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -5,15 +5,24 @@ namespace Bakery
 {
     public class Program
     {
-        public double price = 0;
+        public double orderTotal = 0;
 
         public static void Main()
         {
             Console.WriteLine("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
             Console.WriteLine("Hello! Welcome to Pierre's Bakery!");
             Console.WriteLine("~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~");
+            Console.WriteLine("Would you like to place an order for bread or pastries? ('bread' or 'pastries')");
+            string orderChoice = Console.ReadLine();
+            if (orderChoice == "bread")
+            {
+                Console.WriteLine("How many loaves of bread would you like to order?");
+                int orderNum = int.Parse(Console.ReadLine());
+                Console.WriteLine("You have ordered " + orderNum + " loaves of bread. Would you like to complete your bread order? ('Yes' or 'No')");
+                string answer = Console.ReadLine();
+                
 
-
+            }
         }
     }
 }
