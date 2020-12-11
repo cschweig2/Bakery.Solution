c8818aa305486fa26e7831d5ee66fa263e67d344
Chelsea Becker <cschweig2@gmail.com>
2020-12-11 10:08:03 -0800

    add PastryTests file with boilerplate

---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,13 @@
+using Bakery.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+
+namespace Bakery.Tests
+{
+    [TestClass]
+    public class PastryTests
+    {
+        
+    }
+}
