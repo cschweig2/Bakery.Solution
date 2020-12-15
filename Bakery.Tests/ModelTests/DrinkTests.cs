b658a2edfa928bb84d530b5468694439b81f4a5f
Chelsea Becker <cschweig2@gmail.com>
2020-12-14 17:44:27 -0800

    add test 8 to DrinkTests.cs

--- Bakery.Tests/ModelTests/DrinkTests.cs
+++ Bakery.Tests/ModelTests/DrinkTests.cs
@@ -10,8 +10,16 @@ namespace Bakery.Tests
         [TestMethod]
         public void DrinkConstructor_CreatesInstanceOfDrink_Drink()
         {
-            Drink coffee = new Drink();
+            Drink coffee = new Drink("small", 2);
             Assert.AreEqual(typeof(Drink), coffee.GetType());
+        }
+        [TestMethod]
+        public void GetPrice_GetsPriceOfCoffee_Int()
+        {
+            Drink coffee = new Drink("small", 2);
+            int testPrice = 2;
+            int price = coffee.GetPrice();
+            Assert.AreEqual(testPrice, price);
         }
     }
 }
