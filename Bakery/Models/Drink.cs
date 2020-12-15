cb7149b7dee20305f188e58179d37859602b73a5
Chelsea Becker <cschweig2@gmail.com>
2020-12-14 17:42:11 -0800

    change _price to be a field for testing

--- Bakery/Models/Drink.cs
+++ Bakery/Models/Drink.cs
@@ -6,12 +6,17 @@ namespace Bakery.Models
     public class Drink
     {
         private string _size { get; set; }
-        private int _price { get; set; }
+        private int _price = 2;
 
         public Drink(string size, int price)
         {
             _size = size;
             _price = price;
+        }
+
+        public int GetPrice()
+        {
+            return _price;
         }
     }
 }
