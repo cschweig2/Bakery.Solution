05d91953c5d4770b80f6a7dfa8d99c75a4694bec
Chelsea Becker <cschweig2@gmail.com>
2020-12-16 19:37:17 -0800

    test 10 PASS

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -10,13 +10,13 @@ namespace Bakery.Tests
         [TestMethod]
         public void BreadConstructor_CreatesInstanceOfBread_Bread()
         {
-            Bread bread = new Bread();
+            Bread bread = new Bread(2);
             Assert.AreEqual(typeof(Bread), bread.GetType());
         }
         [TestMethod]
         public void GetPrice_ReturnsPriceOfBread_Int()
         {
-            Bread bread = new Bread();
+            Bread bread = new Bread(2);
             int testPrice = 5;
             bread.SetPrice(5);
             int price = bread.GetPrice();
@@ -25,7 +25,7 @@ namespace Bakery.Tests
         [TestMethod]
         public void SetPrice_SetsPriceOfBread_Int()
         {
-            Bread bread = new Bread();
+            Bread bread = new Bread(2);
             int testPrice = 2;
             int price = bread.SetPrice(2);
             Assert.AreEqual(testPrice, price);
